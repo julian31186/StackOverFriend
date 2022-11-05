432d6a0d1d063b4ea06eca347bd4cf2c7606b283
Julian Elmasry <julian31186@gmail.com>
2022-11-05 19:19:37 -0400

    finish

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require("dotenv").config()
+require('dotenv').config()
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
@@ -8,15 +8,30 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
+
+process.argv.shift()  // skip node.exe
+process.argv.shift()  // skip name of js file
+const arg = process.argv.join(" ");
+if(arg.length == 0) {
+    console.log("Please enter a valid question!")
+    return;
+}
+
 async function AI(input) {
+    try {
     const completion = await openai.createCompletion({
         model: "text-davinci-002",
         prompt: input,
         temperature: 0.6,
+        max_tokens:1500
       });
     
     console.log(completion.data.choices[0].text)
+
+    }catch(error) {
+        console.log("Please Enter Valid Input")
+    }
 }
 
-AI('who is the president of UB');
+AI(arg);
 
